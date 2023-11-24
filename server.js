16cf85cd727f97c033175253ba6f477fb61dcd2c
Itohaneliza <juliuseli2004@gmail.com>
2023-11-24 12:11:30 +0100

    backend

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/ToDoRoute");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.port || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connnected to MOngoDB..."))
+  .catch((err) => console.log(err));
+
+app.use("/api", routes);
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
